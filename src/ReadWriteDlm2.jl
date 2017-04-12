21cea0a4db4498747c5555ea2cee4c57a6bdc729
strickek <strickek@users.noreply.github.com>
2017-04-12 23:51:31 +0200

    Update ReadWriteDlm2.jl

--- src/ReadWriteDlm2.jl
+++ src/ReadWriteDlm2.jl
@@ -11,8 +11,6 @@ export readdlm2, writedlm2
 
 """
 
-The following special readdlm2() methods have been added by module `ReadWriteDlm2`:
-
     readdlm2(source; options...)
     readdlm2(source, T::Type; options...)
     readdlm2(source, delim::Char; options...)
@@ -20,24 +18,24 @@ The following special readdlm2() methods have been added by module `ReadWriteDlm
     readdlm2(source, delim::Char, eol::Char; options...)
     readdlm2(source, delim::Char, T::Type, eol::Char; options...)
 
-They are designed to support the decimal comma parts of the world and 
+Read a matrix from the source where each line (separated by eol) gives one row, with
+elements separated by the given delim. The source can be a text file, stream or byte array. 
+
+`readdlm2()`is designed to support the decimal comma parts of the world and 
 to allow parsing the input strings for Date- and DateTime-types.
 
-By default a preprocessing of input with regex substitution takes place, which
-changes the decimal mark from d,d to d.d. With the keyword argument rs=( , ) 
-another regular expression Tupel can be defined or the regex substitution can 
-be switched of with rs=().
+By default a pre-processing of input with regex substitution takes place, which
+changes the decimal mark from `d,d` to `d.d`. With the keyword argument `rs=( , )`
+another regular expression Tupel can be defined. Regex substitution pre-processing
+can be switched off with: `rs=()`.
 
-The columns are expected to be separated by `';'`, an other `delim`
-can be defined. End of line `eol` is the standard `'\\n'` by default.
+The columns are expected to be separated by `';'`, another `delim`
+can be defined. End of line `eol` is `'\\n'` by default. In addition
+to Base readdlm(), strings are also parsed for ISO Date and DateTime formats
+by default. To switch off Dates parsing set: `dfs="", dtfs=""`.
 
-If all data is numeric, the result will be a numeric array. If some elements
-cannot be parsed as numbers, a heterogeneous array of numbers and strings
-is returned.
-
-In addition to Base readdlm(), strings are also parsed for ISO Date-
-and DateTime formats by default. To switch off Dates parsing set both
-formatstrings in options to: `dfs = \"\", dtfs = \"\"`.
+If all data is numeric, the result will be a numeric array. In other cases
+a heterogeneous array of numbers, dates and strings is returned.
 
 Additional special options with default value are:
 
@@ -119,24 +117,26 @@ function readdlm2auto(input, dlm, T, eol, auto;
 
 """
 
-The following writedlm2() methods have been added by module `ReadWriteDlm2`:
-
     writedlm2(f::IO, A; opts...)
     writedlm2(f::IO, A, delim; opts...)
     writedlm2(filename::AbstractString, A; opts...)
     writedlm2(filename::AbstractString, A, delim; opts...)
 
-They are designed to support the decimal comma parts of the world; they 
+Write A (a vector, matrix, or an iterable collection of iterable rows) as text to f 
+(either a filename string or an IO stream). The columns will be separated by `';'`,
+another `delim` (Char or String) can be defined.
+
+`writedlm2()` is designed to support the decimal comma parts of the world; they 
 also allows to define the format, how Date- and DateTime-types are written.
 
-If keyword argument `decimal=','`(default) a preprocessing of floats takes 
-place, which are parsed to strings with decimal mark changed from `'.'` to `','`.
+By default a pre-processing of floats takes place, which are parsed to strings
+with decimal mark changed from `'.'` to `','`. With a keyword argument
+another decimal mark can be defined, to switch off pre-processing set this to `decimal='.'`.
+Like in Base readdlm(), `writedlm2()` write `3000.0` by default short as `3e3`. To write 
+in the normal print() format set: `write_short=false`.
 
-The columns will be separated by `';'`, an other `delim` can be defined.
-
-In addition to Base writedlm() function the output format for Julia
-Date- and DateTime-data can be defined with format strings. Default are
-the ISO Formats.
+In addition to Base writedlm() function the output format for Date and DateTime data
+can be defined with format strings. Defaults are the ISO Formats.
 
 Additional special options with default value are:
 
