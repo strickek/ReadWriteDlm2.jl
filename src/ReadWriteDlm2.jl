6dd716390563fdb41926aef5f745e096c1fabe95
strickek <strickek@users.noreply.github.com>
2017-04-16 07:43:45 +0200

    Update ReadWriteDlm2.jl
    update docustrings

--- src/ReadWriteDlm2.jl
+++ src/ReadWriteDlm2.jl
@@ -18,40 +18,39 @@ export readdlm2, writedlm2
     readdlm2(source, delim::Char, eol::Char; options...)
     readdlm2(source, delim::Char, T::Type, eol::Char; options...)
 
-Read a matrix from the source where each line (separated by eol) gives one row, with
-elements separated by the given delim. The source can be a text file, stream or byte array. 
+Read a matrix from the source. The `source` can be a text file, stream or byte array.
+Each line (separated by `eol`) gives one row, with elements separated by the given `delim`.  
 
-`readdlm2()`is designed to support the decimal comma parts of the world and 
-to allow parsing the input strings for Date- and DateTime-types.
+The columns are expected to be separated by `';'`, another `delim` can be defined.
 
-By default a pre-processing of input with regex substitution takes place, which
+By default, a pre-processing of input with regex substitution takes place, which
 changes the decimal mark from `d,d` to `d.d`. With the keyword argument `decimal=','`
-the regex Char used by the default regex/substitution Tupel can be changed. With `rs=(..)`
+the regex Char used by the default regex/substitution Tupel can be changed. With `rs=(.., ..)`
 a special regex/substitution Tupel can be defined (in this case `decimal` is not used).
 Regex substitution pre-processing can be switched off with: `rs=()` or `decimal='.'`.
 
-The columns are expected to be separated by `';'`, another `delim`
-can be defined. End of line `eol` is `'\\n'` by default. In addition
-to Base readdlm(), strings are also parsed for ISO Date and DateTime formats
-by default. To switch off Dates parsing set: `dfs="", dtfs=""`.
+End of line `eol` is `'\\n'` by default. In addition
+to Base `readdlm()`, strings are also parsed for ISO Date and DateTime formats
+by default. To switch off parsing Dates formats set: `dfs=\"\", dtfs=\"\"`.
 
 If all data is numeric, the result will be a numeric array. In other cases
 a heterogeneous array of numbers, dates and strings is returned.
 
-Additional keyword arguments with default value are:
+# Additional Keyword Arguments
 
-    decimal=',' (Char used by rs, no function if special rs-Tupel)
-    rs=(r\"(\\d),(\\d)\", s\"\\1.\\2\") (regex (r,s)-Tupel)
-    dfs=\"yyyy-mm-dd\" (format string for Date parsing)
-    dtfs=\"yyyy-mm-ddTHH:MM:SS\" (format string for DateTime)
+* `decimal=','`: decimal mark Char used by default `rs`, irrelevant if `rs`-Tupel is not the default one
+* 'rs=(r\"(\\d),(\\d)\", s\"\\1.\\2\")': Regex (r,s)-Tupel), change `d,d` to `d.d` if `decimale=','`
+* `dfs=\"yyyy-mm-dd\"`: format string for Date parsing, default is ISO
+* 'dtfs=\"yyyy-mm-ddTHH:MM:SS\"': format string for DateTime parsing, default is ISO
 
-Find more information about Base readdlm() functionality and (keyword) arguments -
-which are also supported by `readdlm2()` - in `help` for `readdlm`.
+Find more information about Base `readdlm()` functionality and (keyword) arguments -
+which are also supported by `readdlm2()` - in `help` for `readdlm()`.
 
-Code-Example for reading the Excel(lang=german) textfile `test_de.csv`:
-
-    test = readdlm2(\"test_de.csv\", dfs=\"dd.mm.yyyy\", dtfs=\"dd.mm.yyyy HH:MM\")
-
+# Code-Example 
+for reading the Excel(lang=german) textfile `test_de.csv`:
+```
+test = readdlm2(\"test_de.csv\", dfs=\"dd.mm.yyyy\", dtfs=\"dd.mm.yyyy HH:MM\")
+```
 """
 
 readdlm2(input; opts...) =
@@ -135,37 +134,36 @@ function readdlm2auto(input, dlm, T, eol, auto;
 
     writedlm2(f::IO, A; opts...)
     writedlm2(f::IO, A, delim; opts...)
-    writedlm2(filename::AbstractString, A; opts...)
-    writedlm2(filename::AbstractString, A, delim; opts...)
+    writedlm2(f::AbstractString, A; opts...)
+    writedlm2(f::AbstractString, A, delim; opts...)
 
-Write A (a vector, matrix, or an iterable collection of iterable rows) as text to f 
-(either a filename string or an IO stream). The columns will be separated by `';'`,
-another `delim` (Char or String) can be defined.
+Write `A` (a vector, matrix, or an iterable collection of iterable rows) as text to `f` 
+(either a filename string or an IO stream). 
 
-`writedlm2()` is designed to support the decimal comma parts of the world; they 
-also allows to define the format, how Date- and DateTime-types are written.
+The columns will be separated by `';'`, another `delim` (Char or String) can be defined.
 
-By default a pre-processing of floats takes place, which are parsed to strings
+By default a pre-processing of floats takes place. Floats are parsed to strings
 with decimal mark changed from `'.'` to `','`. With a keyword argument
-another decimal mark can be defined, to switch off pre-processing set this to `decimal='.'`.
-Like in Base readdlm(), `writedlm2()` write `3000.0` by default short as `3e3`. To write 
+another decimal mark can be defined. To switch off this pre-processing set: `decimal='.'`.
+
+Like in `writedlm()` of Base, `writedlm2()` writes `3000.0` by default short as `3e3`. To write 
 in the normal print() format set: `write_short=false`.
 
-In addition to Base writedlm() function the output format for Date and DateTime data
-can be defined with format strings. Defaults are the ISO Formats.
+In `writedlm2()` the output format for Date and DateTime data can be defined with format strings.
+Defaults are the ISO formats.
 
-Additional special options with default value are:
+#Additional Keyword Arguments
 
-    decimal=',' (decimal mark character)
-    write_short=true (use print_shortest() to write)
-    dfs=\"yyyy-mm-dd\" (format string for Date writing)
-    dtfs=\"yyyy-mm-ddTHH:MM:SS\" (format string for DateTime)
+* `decimal=','`: decimal mark character, default is a comma
+* `write_short=true`: Bool - use print_shortest() to write data
+* `dfs=\"yyyy-mm-dd\"': format string, Date write format, default is ISO
+* `dtfs=\"yyyy-mm-ddTHH:MM:SS\"`: format string, DateTime write format, default is ISO
 
-Code-Example for writing the Julia `test` data
-to an text file `test_de.csv` readable by Excel(lang=german):
-
-    writedlm2(\"test_de.csv\", test, dtfs=\"dd.mm.yyyy HH:MM\", dfs=\"dd.mm.yyyy\")
-
+# Code-Example 
+for writing the Julia `test` data to an text file `test_de.csv` readable by Excel(lang=german):
+```
+writedlm2(\"test_de.csv\", test, dtfs=\"dd.mm.yyyy HH:MM\", dfs=\"dd.mm.yyyy\")
+```
 """
 
 
