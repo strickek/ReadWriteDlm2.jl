fb816c67628b292e8460a176714e7c0fc05fc8d2
strickek <strickek@users.noreply.github.com>
2017-05-01 21:23:03 +0200

    Update runtests.jl
    additional test for format strings with variable length

--- test/runtests.jl
+++ test/runtests.jl
@@ -425,3 +425,11 @@ a = [10.9 12.5; Date(2017) DateTime(2017)]
 writedlm2("test.csv", a)
 b = readdlm2("test.csv")
 @test a == b
+
+# test date format strings with variable length (new v0.1.2)
+a = DateTime(2017,5,1,5,59,1)
+writedlm2("test.csv", a, dtfs="E, dd.mm.yyyy H:M:S")
+@test readstring("test.csv") == "Monday, 01.05.2017 5:59:1\n"
+b = readdlm2("test.csv", dtfs="E, dd.mm.yyyy H:M:S")
+rm("test.csv")
+@test b[1] == a
